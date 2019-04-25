709da58d262c79c611ff111e233bd81f02bdd8f7
CorentinHouvin <houvin.corentin@gmail.com>
2019-04-25 09:13:44 +0200

    Ajout css et autre .

--- views/template/header.php
+++ views/template/header.php
@@ -1,14 +1,14 @@
 <header>
 	<a href="/festival">
-		<img src="../assets/img/logo.gif" alt="">
+		<img src="assets/img/logo.gif" alt="">
 	</a>
 
 	<nav>
 		<ul>
-			<li><a href="news" title="">Actualités</a></li>
-			<li><a href="events" title="">Evenements</a></li>
-			<li><a href="movies" title="">Films</a></li>
-			<li><a href="palmares" title="">Palmares</a></li>
+			<li><a href="news" title="" class="<?php if($view == 'news'){echo 'active';}?>">Actualités</a></li>
+			<li><a href="events" title="" class="<?php if($view == 'events'){echo 'active';}?>">Evenements</a></li>
+			<li><a href="movies" title="" class="<?php if($view == 'movies'){echo 'active';}?>">Films</a></li>
+			<li><a href="palmares" title="" class="<?php if($view == 'palmares'){echo 'active';}?>">Palmares</a></li>
 		</ul>
 	</nav>
 </header>
