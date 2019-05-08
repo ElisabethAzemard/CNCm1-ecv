0ec7af02339ac7b23fe3cd641fff492bb35e0481
Maxime <maxlou25@gmail.com>
2019-05-08 22:14:40 +0200

    Page Films et Home

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,77 +4,11 @@ document.addEventListener('DOMContentLoaded', () =>
 {
 	if($('.carousel').length > 0)
 	{
-		/*$.ajax({
-            type: "GET",
-            url: "/festival/assets/json/events.json",
-            dataType: "json",            
-            success: data => {
-                $.each(data.events, function(i, val)
-                {
-					oneEvent = '<div class="carousel-cell">';
-					oneEvent += '<span>' + val.name + '</span>';
-					oneEvent += '</div>';
-					
-                    $('.flickity-slider').append(oneEvent);
-				});
-				
-				$('.main-carousel').flickity({
-					// options
-					cellAlign: 'left',
-					contain: true
-				});
-            },
-            error: err => console.error(err),
-            complete: () => console.log("End of async call") // Option
-		});*/
+		
 	}
 
     if($('#list_movies').length > 0)
     {
-        /*$.ajax({
-            type: "GET",
-            url: "/festival/assets/json/movies.json",
-            dataType: "json",            
-            success: data => {
-                $.each(data.movies, function(i, val)
-                {
-					let class_name = "";
-					let text_category = "";
-
-					switch(val.category)
-					{
-						case 'court_metrage':
-							class_name = 'court_metrage';
-							text_category = 'Court Métrage';
-							break;
-						
-						case 'long_metrage':
-							class_name = 'long_metrage';
-							text_category = 'Long Métrage';
-							break;
-							
-						case 'film_TV':
-							class_name = 'film_TV';
-							text_category = 'Film TV';
-							break;
-					}
-
-					oneMovie = '<div class="oneMovie ' + val.category + '">';
-					oneMovie += '<a href="movies/' + val._id + '">';
-					oneMovie += '<span class="category_movie ' + class_name +'">' + text_category + '</span>';
-					oneMovie += '<span class="like_movie"><img src="' + imagePath + '/like.png" alt="Like"></span>';
-					oneMovie += '<span class="title_movie">' + val.name + '</span>';
-					oneMovie += '<div class="background_image" style="background:url(' + imagePath + val.image + ') center center no-repeat"></div>';
-					oneMovie += '</a>';
-					oneMovie += '</div>';
-					
-                    $('#list_movies').append(oneMovie);
-                });
-            },
-            error: err => console.error(err),
-            complete: () => console.log("End of async call") // Option
-		});*/
-
 		// init Isotope
 		var grid = $('#list_movies').isotope({
 			// options
@@ -85,116 +19,21 @@ document.addEventListener('DOMContentLoaded', () =>
 		{
 			var filterValue = $(this).attr('data-filter');
 			grid.isotope({ filter: filterValue });
-
-			/*$.each($('#list_movies .oneMovie'), function(i, val)
-			{
-				if(filterValue === '*')
-				{
-					$(this).css({'position':'unset','display':'inline-block'});
-				}
-				else
-				{
-					if(!$(this).hasClass(filterValue))
-					{
-						$(this).css({'position':'absolute','display':'none'});
-					}
-					else
-					{
-						$(this).css({'position':'unset','display':'inline-block'});
-					}
-				}
-			});*/
 		});
     }
 
     if($('#movie_detail').length > 0)
     {
-        $.ajax({
-            type: "GET",
-            url: "/festival/assets/json/movies.json",
-            dataType: "json",            
-            success: data => {
-                let id_movie = $('#movie_detail').attr('data-id-movie');
-                
-                $.each(data.movies, function(i, val)
-                {
-                    if(id_movie == val._id)
-                    {
-                        oneMovie = val;
-                    }
-                });
 
-				if(oneMovie !== undefined)
-				{
-					let titleMovie = '<p>Titre original : <b>' + oneMovie.name + '</b></p>';
-					let directors = "";
-
-					$.each(oneMovie.director, function(i, val)
-					{
-						directors += val.givenName + ' ' + val.familyName + ', ';
-					});
-
-					let realisationMovie = '<p>Réalisation : <b>' + directors.substring(0, directors.length - 2) + '</b></p>';
-
-					$('#movie_detail').append(titleMovie);
-					$('#movie_detail').append(realisationMovie);
-					
-					document.title = oneMovie.name + ' - Festival d\'Annecy';
-				}
-			},
-            error: err => console.error(err),
-            complete: () => console.log("End of async call") // Option
-        });
 	}
 	
 	if($('#list_events').length > 0)
     {
-        $.ajax({
-            type: "GET",
-            url: "/festival/assets/json/events.json",
-            dataType: "json",            
-            success: data => {
-                $.each(data.events, function(i, val)
-                {
-                    oneMovie = '<li><a href="events/' + val._id + '"><span>' + val.name + '</span></a></li>';
-                    $('#list_events ul').append(oneMovie);
-                });
-            },
-            error: err => console.error(err),
-            complete: () => console.log("End of async call") // Option
-        });
+     
 	}
 	
 	if($('#event_detail').length > 0)
     {
-        $.ajax({
-            type: "GET",
-            url: "/festival/assets/json/events.json",
-            dataType: "json",            
-            success: data => {
-                let id_event = $('#event_detail').attr('data-id-event');
-
-                $.each(data.events, function(i, val)
-                {
-                    if(id_event == val._id)
-                    {
-                        oneEvent = val;
-                    }
-                });
-
-				if(oneEvent !== undefined)
-				{
-					let titleEvent = '<h2><b>' + oneEvent.name + '</b></h2>';
-					let descriptionEvent = '<p>' + oneEvent.description + '</p>';
-
-					$('#event_detail').append(titleEvent);
-					$('#event_detail').append(descriptionEvent);
-					
-					document.title = oneEvent.name + ' - Festival d\'Annecy';
-				}
-			},
-            error: err => console.error(err),
-            complete: () => console.log("End of async call") // Option
-        });
+        
 	}
 });
--- views/home.php
+++ views/home.php
@@ -1,21 +1,141 @@
-<div class="carousel" data-flickity='{ "autoPlay": true }'>
+<div id="home-page">
+	<img src="./assets/img/annecy_home_1920x470px.jpg" alt="">
+	<p>Le <b>FESTIVAL</b> international du film d’animation d’<b>ANNECY</b> :<br><span>Le rendez-vous mondial de plus de 11 000 professionnels</span></p>
+</div>
+
+<!--<div class="carousel" data-flickity='{ "autoPlay": true }'>
 
 <?php
 
-	$json = file_get_contents('http://localhost/festival/assets/json/events.json');
+	$json = file_get_contents('http://localhost/festival/assets/json/share-events.json');
 	$objEvents = json_decode($json);
 
-	foreach($objEvents->events as $event)
+	foreach($objEvents as $event)
 	{
+		if($event->image != '')
+		{
 	?>
 
 		<div class="carousel-cell">
-			<span><?= $event->name; ?></span>
+			<div class="background-image" style="background:url(<?php echo $event->image; ?>) center center no-repeat">
+				<div class="title-event">
+					<h2><?= $event->name; ?></h2>
+				</div>
+			</div>
 		</div>
 	
 	<?php
+		}
 	}
 
 ?>
 
+</div>-->
+
+<div id="movies-most-like">
+	<h3>Films les plus aimés</h3>
+
+	<?php
+
+		$json = file_get_contents('http://localhost/festival/assets/json/movies.json');
+		$objMovies = json_decode($json);
+		$countMovie = 0;
+
+		foreach($objMovies->movies as $movie)
+		{
+			if($countMovie < 3)
+			{
+				$class_name = "";
+				$text_category = "";
+
+				switch($movie->category)
+				{
+					case 'court_metrage':
+						$class_name = 'court_metrage';
+						$text_category = 'Court Métrage';
+						break;
+					
+					case 'long_metrage':
+						$class_name = 'long_metrage';
+						$text_category = 'Long Métrage';
+						break;
+						
+					case 'film_TV':
+						$class_name = 'film_TV';
+						$text_category = 'Film TV';
+						break;
+				}
+
+				$directorsMovie = $movie->director;
+				$stringDirectors = "";
+
+				foreach($directorsMovie as $director)
+				{
+					$stringDirectors .= $director->givenName . ' ' . $director->familyName . ', ';
+				}
+
+				?>
+				<div class="oneMovie <?= $movie->category; ?>">
+					<div>
+						<div class="background_image" style="background:url(/festival/assets/img/<?= $movie->image; ?>) center center no-repeat">
+							<span class="like_movie"><img src="/festival/assets/img/like.png" alt="Like"></span>
+						</div><!--
+						--><span class="category_movie <?= $class_name; ?>"><?= $text_category; ?></span><!--
+						--><p>Titre :
+								<span class="title_movie"><?= $movie->name; ?></span>
+							</p><!--
+						--><p>Réalisation :
+								<span class="director_movie"><?= substr($stringDirectors, 0, -2); ?></span>
+						</p><!--
+						--><a class="<?= $movie->category; ?>" href="movies/<?= $movie->_id; ?>">Détail</a>
+					</div>
+				</div>
+				<?php
+				$countMovie++;
+			}
+			else
+			{
+				break;
+			}
+		}
+	?>
+
+</div>
+
+<div id="next-events">
+	<h3>Les prochains événements</h3>
+
+	<?php
+
+		$json = file_get_contents('http://localhost/festival/assets/json/share-events.json');
+		$objEvents = json_decode($json);
+		$countEvent = 0;
+
+		foreach($objEvents as $event)
+		{
+			if($countEvent < 3)
+			{
+		?>
+
+			<div class="oneEvent">
+				<div>
+					1
+				</div><!--
+
+				--><div>
+					2
+				</div><!--
+
+				--><div>
+					3
+				</div>
+			</div>
+
+		<?php
+
+				$countEvent++;
+			}
+		}
+
+	?>
 </div>
